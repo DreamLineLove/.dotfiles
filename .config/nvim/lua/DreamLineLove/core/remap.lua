be6a4ee42371d6d7784e68ab67adde9136d0cabf
DreamLineLove <zwenyanzaw@protonmail.com>
2023-11-22 02:23:39 +0630

    more remaps

--- .config/nvim/lua/DreamLineLove/core/remap.lua
+++ .config/nvim/lua/DreamLineLove/core/remap.lua
@@ -7,3 +7,15 @@ keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
+-- In visual mode, move highlighted blocks
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- In normal mode, take line below and append it to current line without moving 
+-- cursor point
+keymap.set("n", "J", "mzJ`z")
+
+-- When half-page jumping, cursor stays in the middle
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
