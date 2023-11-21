2aaa0b06c7955612eb8c65ff56ea353df45f7e64
DreamLineLove <zwenyanzaw@protonmail.com>
2023-11-22 02:59:03 +0630

    more remaps

--- .config/nvim/lua/DreamLineLove/core/remap.lua
+++ .config/nvim/lua/DreamLineLove/core/remap.lua
@@ -25,11 +25,34 @@ keymap.set("n", "<C-u>", "<C-u>zz")
 keymap.set("n", "n", "nzzzv")
 keymap.set("n", "N", "Nzzzv")
 
--- greatest remap ever
+-- Greatest remap ever
 -- watch https://www.youtube.com/watch?v=w7i4amO_zaE 26:33 seconds 
+-- tldr: paste over highlighted word without copying it to current register
 keymap.set("x", "<leader>p", [["_dP]])
 
--- next greatest remap ever : asbjornHaland
+-- Next greatest remap ever : asbjornHaland
 -- (leader y to copy into the system clipboard in both normal and visual modes)
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+keymap.set({"n", "v"}, "<leader>y", [["+y]])
+keymap.set("n", "<leader>Y", [["+Y]])
+
+-- In either normal or visual mode, deletes into the void register
+keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+-- This is going to get me cancelled
+-- keymap.set("i", "<C-c>", "<Esc>")
+
+-- keymap.set("n", "Q", "<nop>")
+
+-- Tmux-sessionizer
+keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- Quick Fix navigation
+--keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+--keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+--keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+--keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- Replace the word under cursor in the whole file with prompted word
+keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
