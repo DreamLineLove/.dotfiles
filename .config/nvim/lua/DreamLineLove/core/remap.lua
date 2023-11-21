f3042b51e8d5476855897503cc2ab32a6b0ed952
DreamLineLove <zwenyanzaw@protonmail.com>
2023-11-22 02:43:27 +0630

    more remaps

--- .config/nvim/lua/DreamLineLove/core/remap.lua
+++ .config/nvim/lua/DreamLineLove/core/remap.lua
@@ -17,5 +17,19 @@ keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 keymap.set("n", "J", "mzJ`z")
 
 -- When half-page jumping, cursor stays in the middle
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- When cycling (forwards or backwards) through search terms, cursor stays in the 
+-- middle
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
+
+-- greatest remap ever
+-- watch https://www.youtube.com/watch?v=w7i4amO_zaE 26:33 seconds 
+keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+-- (leader y to copy into the system clipboard in both normal and visual modes)
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
