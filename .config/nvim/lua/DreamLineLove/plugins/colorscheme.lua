ac582a9dc34d61570fae834764372ba3d56ed7fb
DreamLineLove <zwenyanzaw@protonmail.com>
2023-11-24 17:05:41 +0630

    update colorscheme.lua

--- .config/nvim/lua/DreamLineLove/plugins/colorscheme.lua
+++ .config/nvim/lua/DreamLineLove/plugins/colorscheme.lua
@@ -6,9 +6,12 @@ return {
     name = "gruvbox",
 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
 	priority = 1000, -- make sure to load this before all the other start plugins
+    disable_background = true,
 	config = function()
 		-- load the colorscheme here
 		vim.cmd([[colorscheme gruvbox]])
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 	end,
 
 }
